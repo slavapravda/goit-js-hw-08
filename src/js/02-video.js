cfbe56b70f88f756c1b5e3006868e52dc7c1a4bb
Slava Pravda <pravdaslava7@gmail.com>
2022-07-12 15:43:37 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,14 @@
+import localstorageApi from './localstorage';
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+const IFRAME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localstorageApi.save(IFRAME_KEY, data.seconds);
+};
+player.setCurrentTime(localstorageApi.load(IFRAME_KEY));
+
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import localstorageApi from './localstorage';
+import throttle from 'lodash.throttle';
 
+const FORM_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const userData = {};
+
+const fillFormFields = () => {
+  const userFormLS = localstorageApi.load(FORM_KEY);
+
+  if (userFormLS === undefined) {
+    return;
+  }
+
+  const formElemetns = form.elements;
+
+  for (const key in userFormLS) {
+    if (userFormLS.hasOwnProperty(key)) {
+      formElemetns[key].value = userFormLS[key];
+    }
+  }
+};
+
+const onFormInput = evt => {
+  const target = evt.target;
+
+  console.log(target);
+
+  const formValue = target.value;
+  const formName = target.name;
+
+  userData[formName] = formValue;
+  localstorageApi.save(FORM_KEY, userData);
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  localstorageApi.remove(FORM_KEY);
+  event.currentTarget.reset();
+};
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+fillFormFields();
